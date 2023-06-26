5750bb823db351057ab35991bc86185fd6239bb7
ellie <flowerskullxoxo@gmail.com>
2023-06-26 14:51:11 -0700

    movies algorithm completely working

--- server/api/getCommonMovie.js
+++ server/api/getCommonMovie.js
@@ -5,19 +5,24 @@ const { Casts, Movie, castsMovieLink } = require("../db");
 async function getCommonMovie(actor1, actor2) {
     try {
       // Find movies involving actor1
-      const actor1Movies = await Movie.findAll({
+      const commonMovies = await Movie.findOne({
         include: [
           {
             model: Casts,
             where: {
-              id: actor1,
+              id: [actor1, actor2]
+              
             },
           },
         ],
       });
       // Find movies involving actor2 and also appeared in actor1's movies
-      const commonMovies = actor1Movies.filter(movie => movie.casts.includes(actor2));
-      console.log(commonMovies)
+
+
+    //   const commonMovies = actor1Movies.filter(movie => 
+    //     {console.log(movie.dataValues.casts.dataValues);
+    //         return movie.dataValues.casts.includes(actor2)});
+       console.log(commonMovies)
       return commonMovies;
     } catch (error) {
       console.error('Error retrieving common movies:', error);
