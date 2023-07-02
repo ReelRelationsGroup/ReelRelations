79b9e8a54a97d2d86e923f46cf6bcafa09cea368
chee86j <jeffreychee86@gmail.com>
2023-07-02 14:04:58 -0400

    github login

--- server/app.js
+++ server/app.js
@@ -1,25 +1,92 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const Movie = require("./db/Movie");
 const Casts = require("./db/Casts");
 const degreesOfSeparation = require("./api/degreesOfSeparation");
+const passport = require("passport");
+const GitHubStrategy = require("passport-github2").Strategy;
+const session = require("express-session");
+
+app.use((req, res, next) => {
+  console.log(req.url), next();
+});
 
 app.use(express.json());
 
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_REDIRECT_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      //   return done(err, user);
+      // });
+      console.log(accessToken);
+      done(null, profile);
+    }
+  )
+);
+
+app.use(
+  session({
+    secret: process.env.GITHUB_CLIENT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  console.log(req.isAuthenticated()); // check if user is authenticated from passport
+  next();
+});
+
 app.use("/dist", express.static(path.join(__dirname, "../dist")));
 app.use("/static", express.static(path.join(__dirname, "../static")));
+
+app.get("/", (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect("/login"); // redirect to login if not authenticated
+  }
+});
+
+app.get("/login", passport.authenticate("github", { scope: ["user:email"] }));
 
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "../static/index.html"))
 );
 
+app.get(
+  "/api/auth/oauth/github",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+
 app.use("/api/auth", require("./api/auth"));
 app.use("/api/degreesOfSeparation", degreesOfSeparation);
-app.use("/api/movies", require("./api/movies"))
+app.use("/api/movies", require("./api/movies"));
 app.use("/api/actors", require("./api/actors"));
-app.use("/api/users", require("./api/user"))
-app.use("/api/favorites", require("./api/favorites"))
-
+app.use("/api/users", require("./api/user"));
+app.use("/api/favorites", require("./api/favorites"));
 
 module.exports = app;
